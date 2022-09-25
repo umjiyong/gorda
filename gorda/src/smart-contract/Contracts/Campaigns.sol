3dffa837f9c0587f733697d72d913ee55111fc3b
김신철 <zzangsin2002@gmail.com>
2022-09-25 20:56:43 +0900

    feat : 스마트 컨트랙트 버전 업 스마트 컨트랙트 0.8.0로 버전 업

--- gorda/src/smart-contract/Contracts/Campaigns.sol
+++ gorda/src/smart-contract/Contracts/Campaigns.sol
@@ -1,14 +1,14 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.0;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
-    function createCampaign(uint minimum,string name,string description,string image,uint target) public {
-        address newCampaign = new Campaign(minimum, msg.sender,name,description,image,target);
+    function createCampaign(uint minimum, string memory name, string memory description, string memory image, uint target) public {
+        address newCampaign = address(new Campaign(minimum, msg.sender, name, description, image, target));
         deployedCampaigns.push(newCampaign);
     }
 
-    function getDeployedCampaigns() public view returns (address[]) {
+    function getDeployedCampaigns() public view returns (address[] memory) {
         return deployedCampaigns;
     }
 }
@@ -41,7 +41,7 @@ contract Campaign {
       _;
   }
 
-  function Campaign(uint minimun, address creator,string name,string description,string image,uint target) public {
+  constructor (uint minimun, address creator, string memory name, string memory description, string memory image, uint target) public {
       manager = creator;
       minimunContribution = minimun;
       CampaignName=name;
@@ -58,16 +58,13 @@ contract Campaign {
       approversCount++;
   }
 
-  function createRequest(string description, uint value, address recipient) public restricted {
-      Request memory newRequest = Request({
-         description: description,
-         value: value,
-         recipient: recipient,
-         complete: false,
-         approvalCount: 0
-      });
-
-      requests.push(newRequest);
+  function createRequest(string memory description, uint value, address recipient) public restricted {
+      Request storage newRequest = requests.push();
+      newRequest.description = description;
+      newRequest.value = value;
+      newRequest.recipient = recipient;
+      newRequest.complete = false;
+      newRequest.approvalCount = 0;
   }
 
   function approveRequest(uint index) public {
@@ -82,16 +79,16 @@ contract Campaign {
       require(requests[index].approvalCount > (approversCount / 2));
       require(!requests[index].complete);
 
-      requests[index].recipient.transfer(requests[index].value);
+      payable(requests[index].recipient).transfer(requests[index].value);
       requests[index].complete = true;
 
   }
 
 
-    function getSummary() public view returns (uint,uint,uint,uint,address,string,string,string,uint) {
+    function getSummary() public view returns (uint, uint, uint, uint, address, string memory, string memory, string memory, uint) {
         return(
             minimunContribution,
-            this.balance,
+            address(this).balance,
             requests.length,
             approversCount,
             manager,
