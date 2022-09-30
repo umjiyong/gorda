f24beaaf1cc1e6cd38c6e19cf2f3a8dd0aebbc3c
엄지용 <dogdg23@naver.com>
2022-09-30 11:45:09 +0900

    FIX : User - Login part fixed

--- gorda-be/src/main/java/com/ssafy/gorda/controller/UserController.java
+++ gorda-be/src/main/java/com/ssafy/gorda/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.ssafy.gorda.controller;
 
 import com.ssafy.gorda.domain.User;
+import com.ssafy.gorda.dto.controllerdto.request.LoginUserRequestDto;
 import com.ssafy.gorda.dto.controllerdto.response.LoginUserResponseDto;
 import com.ssafy.gorda.service.UserService;
 import com.ssafy.gorda.util.SHA256;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,18 +24,21 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public LoginUserResponseDto login(String userAccount) {
+    public LoginUserResponseDto login(@RequestBody LoginUserRequestDto request) {
 
-        User user = userService.findByAccount(userAccount);
+        User user = userService.findByAccount(request.getUserAccount());
 
         if (user==null) {             //미등록 유저일 때
 
             User newUser = User.builder()
-                    .userAccount(userAccount)
-                    .userNickname(userService.makeNickname())
+                    .userAccount(request.getUserAccount())
+                    .userNickname(request.getUserNickname())
+                    .userAmount(request.getUserAmount())
+                    .userRole(request.getUserRole())
+                    .userScore(request.getUserScore())
                     .build();
 
-            userService.regist (newUser);
+            userService. regist (newUser);
 
             return new LoginUserResponseDto(newUser);
         }
