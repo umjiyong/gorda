0a9e8f2cb494ba56950ecc623b5c49dc5f7e6f39
엄지용 <dogdg23@naver.com>
2022-09-30 11:45:35 +0900

    FIX : User - Login part fixed

---
+++ gorda-be/src/main/java/com/ssafy/gorda/dto/controllerdto/request/LoginUserRequestDto.java
@@ -0,0 +1,14 @@
+package com.ssafy.gorda.dto.controllerdto.request;
+
+import lombok.Data;
+
+@Data
+public class LoginUserRequestDto {
+
+    private String userAccount;
+    private String userNickname;
+    private int userAmount;
+    private int userScore;
+    private String userRole;
+
+}
