c6f9499b0196b7d5ac87772e90ea2a00de42787a
pch8349 <chan449@naver.com>
2022-10-03 20:15:26 +0900

    Fix merge conflicts

--- gorda/src/funcional/LoginFunc.js
+++ gorda/src/funcional/LoginFunc.js
@@ -1,4 +1,20 @@
-import { useEffect, useState } from "react";
 import Web3 from "web3";
-import { signIn } from "../api/Users";
 
+export const detectCurrentProvider = () => {
+    let provider;
+    if (window.ethereum) {
+        provider = window.ethereum;
+    } else if (window.web3) {
+        // eslint-disable-next-line
+        provider = window.web3.currentProvider;
+    } else {
+        console.log("Non-Ethereum browser detected. You should consider trying MetaMask!");
+    }
+    return provider;
+};
+
+// export const onDisconnect = () => {
+//     localStorage.clear();
+//     setUserInfo({});
+//     setIsConnected(false);
+// };
