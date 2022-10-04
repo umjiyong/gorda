4f995a7eff729fa205d20356012fba0d52a8a550
chan449 <chan449@naver.com>
2022-10-04 18:08:58 +0900

    fix: userLogin fix

--- gorda-be/src/main/java/com/ssafy/gorda/dto/controllerdto/request/LoginUserRequestDto.java
+++ gorda-be/src/main/java/com/ssafy/gorda/dto/controllerdto/request/LoginUserRequestDto.java
@@ -5,7 +5,6 @@ import lombok.Data;
 @Data
 public class LoginUserRequestDto {
 
-    private String userIdx;
     private String userAccount;
     private String userNickname;
     private double userAmount;
