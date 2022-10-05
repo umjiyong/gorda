4aaaf35adb3e7ec8553279417ad4c2e0febfa437
김신철 <zzangsin2002@gmail.com>
2022-10-05 11:24:50 +0900

    tmp

--- gorda-fe/src/smart-contract/donate-contract/factory.js
+++ gorda-fe/src/smart-contract/donate-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  "0x39d7e3D37b011a38251658bFDc47E0D9dc05B716"
+  "0x902e8a3017b02CD69b08884e2F467467D66C7241"
 );
 
 console.log("instance", instance);
