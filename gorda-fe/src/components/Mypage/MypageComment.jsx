3c2620852e0fe598afbba2b16e0519f93309d96b
DESKTOP-KVCQHCD\SSAFY <rlaxorud814@gmail.com>
2022-10-06 16:39:54 +0900

    fix

--- gorda-fe/src/components/Mypage/MypageComment.jsx
+++ gorda-fe/src/components/Mypage/MypageComment.jsx
@@ -5,50 +5,42 @@ import "./MypageComment.scss";
 import MypageCommentList from "./MypageCommentList";
 
 function MypageComment() {
-    const [comment, setComment] = useState([]);
-    console.log(comment)
-    const getCommentCnt = async() => {
-        await getComment({userIdx: localStorage.getItem("idx")},
-        (response) => {
-            setComment(response.data.data);
-            console.log(comment)
-        },
-        (err) => {
-            console.log(err);
-        })
-    };
-
-    useEffect(() => {
-        getCommentCnt();
-    }, [])
-
-    return (
-        <>
-            <div className="comment_container">
-                <div className="comment_title">
-                    내 댓글 <span>{comment.length}</span>
-                </div>
-<<<<<<< HEAD
-                    {comment.map((value, index) => (
-                 <Link to={`/detail/${comment[index].donationIdx}`}>
-                        <MypageCommentList 
-                        index = {index}
-                        donationIdx = {comment[index].donationIdx}
-                    />
-                </Link>
-                    ))}   
-=======
-                
-                {comment.map((value, index) => (
-                    <MypageCommentList 
-                    index = {value}
-                    donationIdx = {comment[index].donationIdx}
-                />
-                ))}
->>>>>>> 08df72d (add: dummy data)
-            </div>
-        </>
+  const [comment, setComment] = useState([]);
+  console.log(comment);
+  const getCommentCnt = async () => {
+    await getComment(
+      { userIdx: localStorage.getItem("idx") },
+      (response) => {
+        setComment(response.data.data);
+        console.log(comment);
+      },
+      (err) => {
+        console.log(err);
+      }
     );
+  };
+
+  useEffect(() => {
+    getCommentCnt();
+  }, []);
+
+  return (
+    <>
+      <div className="comment_container">
+        <div className="comment_title">
+          내 댓글 <span>{comment.length}</span>
+        </div>
+        {comment.map((value, index) => (
+          <Link to={`/detail/${comment[index].donationIdx}`}>
+            <MypageCommentList
+              index={index}
+              donationIdx={comment[index].donationIdx}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default MypageComment;
